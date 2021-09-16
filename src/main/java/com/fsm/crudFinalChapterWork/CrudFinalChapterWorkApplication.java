1c45c5e86639e6deb584f2527af1345543abfe90
fabio21777 <fabiosouza21777@gmail.com>
2021-09-16 15:10:10 -0300

    iniciando o projeto

---
+++ src/main/java/com/fsm/crudFinalChapterWork/CrudFinalChapterWorkApplication.java
@@ -0,0 +1,13 @@
+package com.fsm.crudFinalChapterWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFinalChapterWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFinalChapterWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsm/crudFinalChapterWork/CrudFinalChapterWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsm.crudFinalChapterWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFinalChapterWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
